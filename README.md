09e3cfa432fb766a868865e19692e5f586c9e826
j.clevenger <j.clevenger@cgiar.org>
2024-04-08 20:34:00 -0500

    updates for v25 and additional config

--- README.md
+++ README.md
@@ -3,18 +3,17 @@
 ## Docker compose runtime for BMS - IBP's [Breeding Management System](https://github.com/IntegratedBreedingPlatform)
 
 Get the BMS war files from IBP or build using https://github.com/jamesclevenger/BMS-Build-Open and copy them to this dir.
+war files:
+bmsapi.war
+Fieldbook.war
+ibpworkbench.war
+inventory-manager.war
 
-Create a .env file, replace with the appropriate BMS release version, your mysql password, & crop(s)
-```
-BMS_RELEASE=24.1.0
-BMS_DB_HOST=bmsmysql
-BMS_DB_PASS=<your-mysql-password-here>
-BMS_CROPS=maize
-BMS_MEMORY=2g
-LIQUIBASE_PARAM=-Dspring.profiles.active=development
-```
+Create a .env file; you can rename env.sample to .env and use that as a template - replacing with the appropriate BMS release version, your mysql password, crop(s), etc
 
 Start the runtime with
 ```bash
 docker compose up
 ```
+
+browse to http://localhost:8888/ibpworkbench/main
--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 if [[ ! -d /var/lib/mysql/workbench ]]; then
     echo "=> Fresh BMS Install Detected, creating initial DB"
-    echo "=> Installing workbench and maize ..."
+    echo "=> Installing workbench and ${BMS_CROPS:-maize} ..."
+    echo "the BMS_DB_HOST var is ${BMS_DB_HOST}"
     cd /bms/DBScripts/setuputils
     groovy createworkbenchschema.groovy bmsdocker
     CROPS="${BMS_CROPS:-maize}"
@@ -12,5 +13,38 @@ if [[ ! -d /var/lib/mysql/workbench ]]; then
 	done
     echo "=> Done!"
 fi
+
+cat << EOF > /usr/local/tomcat/webapps/bmsapi/WEB-INF/classes/database.properties
+db.host=${BMS_DB_HOST}
+db.password=${BMS_DB_PASSWORD}
+EOF
+
+cat << EOF >> /usr/local/tomcat/webapps/bmsapi/WEB-INF/classes/application.properties
+active.users.max.number=${BMS_ACTIVE_USERS_MAX_NUMBER}
+EOF
+
+cat << EOF > /usr/local/tomcat/webapps/ibpworkbench/WEB-INF/classes/database.properties
+db.host=${BMS_DB_HOST}
+db.password=${BMS_DB_PASSWORD}
+EOF
+
+cat << EOF > /usr/local/tomcat/webapps/Fieldbook/WEB-INF/classes/database.properties
+db.host=${BMS_DB_HOST}
+db.password=${BMS_DB_PASSWORD}
+EOF
+
+#mail.server.host
+sed -i "s/mail.server.host=$/mail.server.host=${MAIL_SERVER_HOST}/" /usr/local/tomcat/webapps/ibpworkbench/WEB-INF/classes/workbench.properties
+#mail.server.port
+sed -i "s/mail.server.port=$/mail.server.port=${MAIL_SERVER_PORT}/" /usr/local/tomcat/webapps/ibpworkbench/WEB-INF/classes/workbench.properties
+#mail.server.protocol
+sed -i "s/mail.server.protocol=$/mail.server.protocol=${MAIL_SERVER_PROTOCOL}/" /usr/local/tomcat/webapps/ibpworkbench/WEB-INF/classes/workbench.properties
+#mail.server.username
+sed -i "s/mail.server.username=.*$/mail.server.username=${MAIL_SERVER_USERNAME}/" /usr/local/tomcat/webapps/ibpworkbench/WEB-INF/classes/workbench.properties
+#mail.server.sender.email
+sed -i "s/mail.server.sender.email=.*$/mail.server.sender.email=${MAIL_SERVER_SENDER_EMAIL}/" /usr/local/tomcat/webapps/ibpworkbench/WEB-INF/classes/workbench.properties
+#mail.server.password
+sed -i "s/mail.server.password=$/mail.server.password=${MAIL_SERVER_PASSWORD}/" /usr/local/tomcat/webapps/ibpworkbench/WEB-INF/classes/workbench.properties
+
 
 exec $CATALINA_HOME/bin/catalina.sh run
